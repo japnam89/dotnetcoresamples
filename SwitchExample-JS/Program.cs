f3ace004dc3555d6143e296481851d5d24c9f0ba
JAPNAM SINGH <japnam.singh@gmail.com>
2020-01-11 11:09:00 -0500

    Using C# Version 8 We can implement Switch expression which is very friendly to use
    Using C# Version 8
    We can implement Switch expression which is very friendly to use

---
+++ SwitchExample-JS/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SwitchExample_JS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caseSwitch = 1;
+            var result="";
+
+            switch (caseSwitch)
+            {
+                case 1:
+                    result = "Case 1";
+                    break;
+                case 2:
+                    result = "Case 2";
+                    break;
+                case 3:
+                    result = "Case 3";
+                    break;
+                default:
+                    result = "Default Case";
+                    break;
+            }
+            Console.WriteLine(result);
+
+
+            //Now using c# 8 switch expression
+
+            var caseSwitch2 = 2;
+            var result2 = "";
+            result2 = caseSwitch2 switch
+
+            {
+                1 => "Case 1",
+                2 => "Case 2",
+                3 => "Case 3 ",
+                _ => "Default Case"
+            };
+
+            Console.WriteLine("Using c#8 Switch expression:\n"+ result2);
+
+        }
+    }
+}
+  
